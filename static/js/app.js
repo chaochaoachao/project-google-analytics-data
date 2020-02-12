62e7515abc6c25830ba2916096bde6e2ff89d0a5
lifeten <54412012+lifeten@users.noreply.github.com>
2020-02-12 12:40:47 -0500

    final_geo_change

--- static/js/app.js
+++ static/js/app.js
@@ -48,18 +48,13 @@ function GEO_Plot(Country_name,COUNTRY_count){
     z: COUNTRY_count,
     text: Country_name,
     autocolorscale: true,
-    colorscale: [
-      [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
-      [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
-      [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
-  ],
     colorbar: {
       title: 'country count',
       thickness: 0.2
   },
   marker: {
     line:{
-        color: 'rgb(255,255,255)',
+        color: 'rgb(0,0,0)',
         width: 2
     }
 }
