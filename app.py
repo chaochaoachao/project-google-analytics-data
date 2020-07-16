359b647ab74ca2cc3e6532031ee84993b34955ab
lifeten <54412012+lifeten@users.noreply.github.com>
2020-07-16 16:47:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config.update(
     MAIL_USE_TLS = False,
     MAIL_USERNAME = 'dq177000@gmail.com',
     MAIL_PASSWORD = 'wjraoggfdptjmhzi',
-    MAIL_DEFAULT_SENDER ='dq177000@gmail.com'
+    MAIL_DEFAULT_SENDER =
 )
 
 var email_group = [{email: "di.qu@mail.utoronto.ca",id:"1957458976293878000"},
